d628b71a9413cdcfa9f6227dbe39293bab9cc666
mateusz5564 <mateusz5564@gmail.com>
2022-02-03 22:06:27 +0100

    make Container more reusable

--- components/shared/Container.js
+++ components/shared/Container.js
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
-  max-width: 144rem;
+  max-width: ${({ $maxWidth }) => ($maxWidth ? $maxWidth : "120rem")};
+  text-align: ${({ $textAlign }) => ($textAlign ? $textAlign : "initial")};
   margin: 0 auto;
 `;
- 
+
 export default Container;
