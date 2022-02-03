bb4c02facd0ff3a1154fea6f8fb2c6a905873b1f
mateusz5564 <mateusz5564@gmail.com>
2022-02-03 22:10:43 +0100

    import images for swipers

---
+++ utils/museumsImages.js
@@ -0,0 +1,13 @@
+import museumImg2 from '../public/img/museums/2.png';
+import museumImg3 from '../public/img/museums/3.jpg';
+import museumImg4 from '../public/img/museums/4.jpg';
+import museumImg5 from '../public/img/museums/5.jpg';
+
+const museumsImages = [
+  museumImg2,
+  museumImg3,
+  museumImg4,
+  museumImg5,
+];
+
+export default museumsImages;
---
+++ utils/regionImages.js
@@ -0,0 +1,31 @@
+import region1Img from "../public/img/region/1.jpg";
+import region2Img from "../public/img/region/2.jpg";
+import region3Img from "../public/img/region/3.jpg";
+import region4Img from "../public/img/region/4.jpg";
+import region5Img from "../public/img/region/5.jpg";
+import region6Img from "../public/img/region/6.jpg";
+import region7Img from "../public/img/region/7.jpg";
+import region8Img from "../public/img/region/8.jpg";
+import region9Img from "../public/img/region/9.jpg";
+import region10Img from "../public/img/region/10.jpg";
+import region11Img from "../public/img/region/11.jpg";
+import region12Img from "../public/img/region/12.jpg";
+import region13Img from "../public/img/region/13.jpg";
+
+const regionImages = [
+  region1Img,
+  region2Img,
+  region3Img,
+  region4Img,
+  region5Img,
+  region6Img,
+  region7Img,
+  region8Img,
+  region9Img,
+  region10Img,
+  region11Img,
+  region12Img,
+  region13Img,
+];
+
+export default regionImages;
