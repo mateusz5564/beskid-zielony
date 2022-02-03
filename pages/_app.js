4e13775e1e11ec57bcea2a6f19a7e1efe3dbf291
mateusz5564 <mateusz5564@gmail.com>
2022-02-03 22:12:46 +0100

    add Header and Footer to layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/GlobalStyle";
 import theme from '../styles/theme';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
