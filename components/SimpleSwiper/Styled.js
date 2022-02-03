fb7fd19b489b5a2ae4d97bc3d51608c5270e860c
mateusz5564 <mateusz5564@gmail.com>
2022-02-03 22:11:35 +0100

    add SimpleSwiper

---
+++ components/SimpleSwiper/Styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Swiper } from "swiper/react";
+
+const StyledSwiper = styled(Swiper)`
+  img {
+    height: 20rem;
+  }
+`;
+
+const Styled = { StyledSwiper };
+
+export default Styled;
---
+++ components/SimpleSwiper/index.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Autoplay } from "swiper";
+import { SwiperSlide } from "swiper/react";
+import S from "./Styled";
+import "swiper/css";
+
+const SimpleSlider = ({ images, slidesPerView }) => {
+  return (
+    <S.StyledSwiper
+      modules={[Autoplay]}
+      slidesPerView={slidesPerView}
+      spaceBetween={24}
+      autoplay={{ delay: 5000, pauseOnMouseEnter: true, disableOnInteraction: false }}
+      loop
+    >
+      {images.map((imgSrc, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <Image src={imgSrc} alt="" />
+          </SwiperSlide>
+        );
+      })}
+    </S.StyledSwiper>
+  );
+};
+
+export default SimpleSlider;
