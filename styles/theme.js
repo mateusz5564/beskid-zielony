f11a144b303c936541d6128ca09b6d3970f747d6
mateusz5564 <mateusz5564@gmail.com>
2022-02-03 22:15:33 +0100

    add new colors

--- styles/theme.js
+++ styles/theme.js
@@ -2,10 +2,13 @@ const colors = {
   white: "#ffffff",
   whiteGreen: '#EAFDEE',
   darkGreen: {
+    700: '#014520',
     500: '#017939',
+    50: '#8DD9B0',
   },
   lightGreen: {
     500: '#68B92E',
+    100: '#C1F29D',
     50: '#E1FFCC',
   },
   darkYellow: {
@@ -16,6 +19,7 @@ const colors = {
   },
   darkGray: {
     600: '#2B4034',
+    300: '#739985',
   }
 };
 
