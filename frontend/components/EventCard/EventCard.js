181275ef103a0d4dcbecc15a263ae53819135f2e
mateusz5564 <mateusz5564@gmail.com>
2022-02-15 15:17:34 +0100

    display proper date in EventCard

--- frontend/components/EventCard/EventCard.js
+++ frontend/components/EventCard/EventCard.js
@@ -2,28 +2,29 @@ import S from "./Styled";
 import { getFormattedDate, getFormattedTime } from "../../utils/formatDate";
 
 const EventsCard = ({ event }) => {
-
   return (
     <S.Wrapper>
       <div className="top">
         <div className="thumbnail">
-          <img  src={`http://localhost:1337${event.miniaturka}`} alt="" />
+          <img src={`http://localhost:1337${event.miniaturka}`} alt="" />
         </div>
         <div className="right">
           <div>
             <h4 className="title">{event.tytul}</h4>
             <p className="detailWithIcon">
               <img className="icon" src="/img/icons/calendar.png" alt="" />
-              {`${getFormattedDate(event.termin)}, ${getFormattedTime(event.termin)}`}
+              {!event.czasowe &&
+                `${getFormattedDate(event.termin)}, ${getFormattedTime(event.termin)}`}
+              {event.czasowe && `do ${getFormattedDate(event.termin)}`}
             </p>
             <p className="detailWithIcon">
               <img className="icon" src="/img/icons/location.png" alt="" />
               {event.miejscowosc}, {event.lokalizacja}
             </p>
-          <p className="detailWithIcon test">
-            <img className="icon" src="/img/icons/web.png" alt="" />
-            {event.stronaInternetowa}
-          </p>
+            <p className="detailWithIcon">
+              <img className="icon" src="/img/icons/web.png" alt="" />
+              {event.stronaInternetowa}
+            </p>
             <p className="ticket">wstęp: {event.wstep}</p>
           </div>
         </div>
