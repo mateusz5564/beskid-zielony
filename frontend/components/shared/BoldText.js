de544715ec40bef36f114b0916506e5069af0d02
mateusz5564 <mateusz5564@gmail.com>
2022-02-16 19:49:15 +0100

    add font weight props

--- frontend/components/shared/BoldText.js
+++ frontend/components/shared/BoldText.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
-const BoldText = ({ children }) => {
-  return <Bold>{children}</Bold>;
+const BoldText = ({ children, weight }) => {
+  return <Bold $weight={weight}>{children}</Bold>;
 };
 
 const Bold = styled.b`
-  font-weight: 700;
+  font-weight: ${({$weight}) => $weight ? $weight : '700'};
 `;
 
 export default BoldText;
