62b43e78a1d592e866272d89a37ad8671fb84cb8
mateusz5564 <mateusz5564@gmail.com>
2022-02-16 19:50:33 +0100

    add padding

--- frontend/components/shared/Container.js
+++ frontend/components/shared/Container.js
@@ -4,6 +4,7 @@ const Container = styled.div`
   max-width: ${({ $maxWidth }) => ($maxWidth ? $maxWidth : "120rem")};
   text-align: ${({ $textAlign }) => ($textAlign ? $textAlign : "initial")};
   margin: 0 auto;
+  padding: 0 2rem;
 `;
 
 export default Container;
