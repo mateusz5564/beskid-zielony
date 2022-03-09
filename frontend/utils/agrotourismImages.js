e88afd7c12ace8573cb5cc8710d2b0d114870b37
mateusz5564 <mateusz5564@gmail.com>
2022-03-09 14:26:48 +0100

    rename agrotourism images

--- frontend/utils/agrotourismImages.js
+++ frontend/utils/agrotourismImages.js
@@ -1,11 +1,11 @@
 import agroImg1 from "../public/img/agrotourism/1.jpg";
 import agroImg2 from "../public/img/agrotourism/2.jpg";
 import agroImg3 from "../public/img/agrotourism/3.jpg";
+import agroImg4 from "../public/img/agrotourism/4.jpg";
 import agroImg5 from "../public/img/agrotourism/5.jpg";
 import agroImg6 from "../public/img/agrotourism/6.jpg";
 import agroImg7 from "../public/img/agrotourism/7.jpg";
-import agroImg8 from "../public/img/agrotourism/8.jpg";
 
-const agrotourismImages = [agroImg1, agroImg2, agroImg3, agroImg5, agroImg6, agroImg7, agroImg8];
+const agrotourismImages = [agroImg1, agroImg2, agroImg3, agroImg4, agroImg5, agroImg6, agroImg7];
 
 export default agrotourismImages;
