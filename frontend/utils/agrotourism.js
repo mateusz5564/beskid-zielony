5a3c8547f41c0a2ee3934e0b2e3dcbb8d5e56210
mateusz5564 <mateusz5564@gmail.com>
2022-03-16 20:57:44 +0100

    add urls to elements of the ads slider

---
+++ frontend/utils/agrotourism.js
@@ -0,0 +1,19 @@
+import img1 from "../public/img/agrotourism/1.jpg";
+import img2 from "../public/img/agrotourism/2.jpg";
+import img3 from "../public/img/agrotourism/3.jpg";
+import img4 from "../public/img/agrotourism/4.jpg";
+import img5 from "../public/img/agrotourism/5.jpg";
+import img6 from "../public/img/agrotourism/6.jpg";
+import img7 from "../public/img/agrotourism/7.jpg";
+
+const agrotourism = [
+  { img: img1, url: "http://chutorgrzegorza.pl/" },
+  { img: img2, url: "https://chyzahani24.pl/" },
+  { img: img3, url: "https://domnawzgorzu24.pl/" },
+  { img: img4, url: "https://www.magnoliabiecz.pl/" },
+  { img: img5, url: "http://www.agroturystykabartne.pl/" },
+  { img: img6, url: "https://www.udobrychznajomych.com/" },
+  { img: img7, url: "https://www.domkiwbeskidach.pl/" },
+];
+
+export default agrotourism;
---
+++ frontend/utils/museums.js
@@ -0,0 +1,25 @@
+import img2 from "../public/img/museums/2.png";
+import img3 from "../public/img/museums/3.jpg";
+import img4 from "../public/img/museums/4.jpg";
+import img5 from "../public/img/museums/5.jpg";
+
+const museums = [
+  {
+    img: img2,
+    url: "http://www.dziubynilka.pl/",
+  },
+  {
+    img: img3,
+    url: "http://www.muzeumnowyzmigrod.pl/",
+  },
+  {
+    img: img4,
+    url: "https://www.mazurowka.com/",
+  },
+  {
+    img: img5,
+    url: "https://www.uzdrowiskowapienne.pl/index.php/pl/galeria/pijalnia",
+  },
+];
+
+export default museums;
