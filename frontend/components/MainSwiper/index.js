12dace3018d51f52b5f5f95d38a49ee0e1c2d3a8
mateusz5564 <mateusz5564@gmail.com>
2022-03-30 21:54:40 +0200

    add a11y to MainSwiper

--- frontend/components/MainSwiper/index.js
+++ frontend/components/MainSwiper/index.js
@@ -1,18 +1,20 @@
 import Image from "next/image";
-import { Autoplay, EffectCoverflow, Navigation, Pagination} from "swiper";
+import { A11y, Autoplay, EffectCoverflow, Keyboard, Navigation, Pagination } from "swiper";
 import { SwiperSlide } from "swiper/react";
-import S from './Styled';
+import S from "./Styled";
 
 import "swiper/css";
+import "swiper/css/a11y";
+import "swiper/css/autoplay";
+import "swiper/css/keyboard";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-coverflow";
 
 const MainSwiper = ({ images, breakpoints }) => {
-
   return (
     <S.StyledSwiper
-      modules={[Autoplay, EffectCoverflow, Navigation, Pagination]}
+      modules={[A11y, Autoplay, EffectCoverflow, Keyboard, Navigation, Pagination]}
       effect="coverflow"
       centeredSlides={true}
       slidesPerView={1}
@@ -26,10 +28,20 @@ const MainSwiper = ({ images, breakpoints }) => {
       }}
       autoplay={{ delay: 5000, pauseOnMouseEnter: true, disableOnInteraction: false }}
       loop
+      keyboard={{ enabled: true }}
       navigation
       pagination={{ clickable: true }}
+      a11y={{
+        containerMessage: "Zdjęcia przedstawiające beskid niski",
+        firstSlideMessage: "To jest pierwsze zdjęcie",
+        lastSlideMessage: "To jest ostanie zdjęcie",
+        prevSlideMessage: "Poprzednie zdjęcie",
+        nextSlideMessage: "Nastepne zdjęcie",
+        paginationBulletMessage: "Przejdź do zdjęcia nr {{index}}",
+        slideRole: "img",
+      }}
     >
-      {images.map((img) => {
+      {images.map(img => {
         return (
           <SwiperSlide key={img.src}>
             <Image src={img} placeholder="blur" layout="responsive" alt="" />
