355e68b33a804c3bda1df51e2175a91162062289
mateusz5564 <mateusz5564@gmail.com>
2022-04-07 00:21:11 +0200

    add wrapper for skip link and reset focus on page switch

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,33 @@
+import { useRouter } from "next/router";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/GlobalStyle";
 import theme from "../styles/theme";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      document.body.setAttribute("tabindex", "-1");
+      document.body.focus();
+      document.body.removeAttribute("tabindex");
+      console.log(document.activeElement)
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <div id="skip-content"></div>
       <Header />
       <Component {...pageProps} />
       <Footer />
