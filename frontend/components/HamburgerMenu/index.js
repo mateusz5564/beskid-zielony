0b4f907ad53b6420a89924475a2a0e8c40f69f04
mateusz5564 <mateusz5564@gmail.com>
2022-04-11 20:36:54 +0200

    add keytrap to HamburgerMenu

--- frontend/components/HamburgerMenu/index.js
+++ frontend/components/HamburgerMenu/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import NextLink from "next/link";
 import Backdrop from "../shared/Backdrop";
@@ -10,6 +10,9 @@ const HamburgerMenu = () => {
   const pathname = router.pathname;
 
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+  const hamburgerBtnRef = useRef(null);
+  const linksListRef = useRef(null);
+  const activeIndexRef = useRef(0);
 
   const closeModal = () => {
     setIsSideNavOpen(false);
@@ -25,10 +28,67 @@ const HamburgerMenu = () => {
     isSideNavOpen ? closeModal() : openModal();
   };
 
+  const moveFocus = (focusableElements, direction = "down") => {
+    const length = focusableElements.length;
+
+    if (direction === "down") {
+      if (activeIndexRef.current === length - 1) {
+        activeIndexRef.current = 0;
+      } else {
+        activeIndexRef.current++;
+      }
+    }
+    if (direction === "up") {
+      if (activeIndexRef.current === 0) {
+        activeIndexRef.current = length - 1;
+      } else {
+        activeIndexRef.current--;
+      }
+    }
+
+    focusableElements[activeIndexRef.current].focus();
+  };
+
+  const handleKeyboardTrap = e => {
+    const hamburgerBtnEl = hamburgerBtnRef.current;
+    const linksListEl = linksListRef.current;
+    const listItems = [...linksListEl.children];
+    const links = listItems.map(item => item.firstChild);
+
+    const focusableElements = [hamburgerBtnEl, ...links];
+    if (e.keyCode === 9 && !e.shiftKey) {
+      e.preventDefault();
+      moveFocus(focusableElements);
+    }
+    if (e.keyCode === 9 && e.shiftKey) {
+      e.preventDefault();
+      moveFocus(focusableElements, "up");
+    }
+    if (e.keyCode === 27) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    if (isSideNavOpen) {
+      document.addEventListener("keydown", handleKeyboardTrap);
+    }
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardTrap);
+    };
+  }, [isSideNavOpen]);
+
+  useEffect(() => {
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardTrap);
+    };
+  }, []);
+
   return (
     <>
       <S.Hamburger
         id="hamburger-btn"
+        ref={hamburgerBtnRef}
         isSideNavOpen={isSideNavOpen}
         onClick={handleHamburger}
         aria-haspopup="true"
@@ -40,7 +100,7 @@ const HamburgerMenu = () => {
         <span></span>
         <SrOnly>{isSideNavOpen ? "Zamknij menu" : "Otwórz menu"}</SrOnly>
       </S.Hamburger>
-      <S.SideNav id="hamburger-menu" isSideNavOpen={isSideNavOpen}>
+      <S.SideNav id="hamburger-menu" ref={linksListRef} isSideNavOpen={isSideNavOpen}>
         {isSideNavOpen && <Backdrop onClick={closeModal} />}
         <S.SideNavItem>
           <NextLink
